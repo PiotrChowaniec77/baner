c2ae089a99da802b52921f280f9bf11a4302f278
Piotr Chowaniec <piotrchowaniec@poczta.fm>
2019-09-09 13:39:13 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -14,23 +14,43 @@ const slideList = [
 ];
 const image = document.querySelector("img.slider");
 const h1 = document.querySelector("h1.slider");
-const dots = [...document.querySelectorAll('.dots span')]
+const dots = [...document.querySelectorAll(".dots span")];
 
 //  Interfejs
-const time = 500;
+const time = 1500;
 let active = 0;
 const changeDot = () => {
-    const activeDot = dots.findIndex(dot => dot.classList.contains("active"))
-    dots[activeDot].classList.remove('active')
-    dots[active].classList.add('active')
-}
+    const activeDot = dots.findIndex(dot => dot.classList.contains("active"));
+    dots[activeDot].classList.remove("active");
+    dots[active].classList.add("active");
+};
 const changeSlide = () => {
     active++;
     if (active >= slideList.length) {
-        active = 0
+        active = 0;
     }
     image.src = slideList[active].img;
     h1.textContent = slideList[active].text;
-    changeDot()
+    changeDot();
 };
-setInterval(changeSlide, time);
+
+const keyChangeSlide = e => {
+    if (e.keyCode == 37 || e.keyCode == 39) {
+        clearInterval(indexInterval);
+        e.keyCode == 37 ? active-- : active++;
+        console.log(active);
+        if (active === slideList.length) {
+            active = 0;
+        } else if (active < 0) {
+            active = slideList.length - 1;
+        }
+        image.src = slideList[active].img;
+        h1.textContent = slideList[active].text;
+        changeDot();
+        indexInterval = setInterval(changeSlide, time)
+    }
+};
+
+let indexInterval = setInterval(changeSlide, time);
+
+window.addEventListener("keydown", keyChangeSlide);
